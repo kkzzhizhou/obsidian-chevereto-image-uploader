9631c1e10dbbf15f33402b0c4b7953d180efcb17
kkzzhizhou <806508401@qq.com>
2022-02-11 20:54:48 +0800

    fix codemirror

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import {
   Notice,
   Plugin,
-  MarkdownView,
   Editor,
 } from "obsidian";
 
 import axios from "axios"
 import objectPath from 'object-path'
-import ImageUploaderSettingTab from './settings-tab'
 import Compressor from 'compressorjs'
+
+import ImageUploaderSettingTab from './settings-tab'
+
 interface ImageUploaderSettings {
   apiEndpoint: string;
   token: string;
@@ -23,10 +24,6 @@ const DEFAULT_SETTINGS: ImageUploaderSettings = {
   enableResize: false,
 };
 
-type Handlers = {
-  drop: (cm: CodeMirror.Editor, event: DragEvent) => void;
-  paste: (cm: CodeMirror.Editor, event: ClipboardEvent) => void;
-};
 
 async function readFileAsDataURL(file) {
   let result_base64 = await new Promise((resolve) => {
@@ -41,84 +38,54 @@ async function readFileAsDataURL(file) {
 export default class ImageUploader extends Plugin {
   settings: ImageUploaderSettings;
 
-
-  private cmAndHandlersMap = new Map<CodeMirror.Editor, Handlers>();
-
-  private getEditor() {
-    const mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
-    if (mdView) {
-      return mdView.editor;
-    } else {
-      return null;
-    }
-  }
-
-  private backupOriginalHandlers(cm: CodeMirror.Editor) {
-    if (!this.cmAndHandlersMap.has(cm)) {
-      this.cmAndHandlersMap.set(cm, {
-        drop: (cm as any)._handlers.drop[0],
-        paste: (cm as any)._handlers.paste[0],
-      });
-    }
-    return this.cmAndHandlersMap.get(cm);
-  }
-
   setupPasteHandler(): void {
-    this.registerCodeMirror((cm: any) => {
-      const originalPasterHandler = this.backupOriginalHandlers(cm);
-      cm._handlers.paste[0] = async (_: any, e: ClipboardEvent) => {
-        const { files } = e.clipboardData;
-        // console.log(files)
-        if (files.length == 0 || !files[0].type.startsWith("image")) {
-          originalPasterHandler.paste(_, e)
-        }
-        else if (this.settings.apiEndpoint && this.settings.token) {
-          for (let file of files) {
-            const randomString = (Math.random() * 10086).toString(36).substr(0, 8)
-            const pastePlaceText = `![uploading...](${randomString})\n`
-            this.getEditor().replaceSelection(pastePlaceText)
-            const maxWidth = this.settings.maxWidth
-            if (this.settings.enableResize) {
-              const compressedFile = await new Promise((resolve, reject) => {
-                new Compressor(file, {
-                  maxWidth: maxWidth,
-                  success: resolve,
-                  error: reject,
-                })
+    this.registerEvent(this.app.workspace.on('editor-paste', async (evt: ClipboardEvent, editor: Editor) => {
+      const { files } = evt.clipboardData;
+      // console.log(files)
+      if (files.length == 0 || files[0].type.startsWith("text")) {
+        return
+      } else if (this.settings.apiEndpoint && this.settings.token) {
+        for (let file of files) {
+          evt.preventDefault();
+          const randomString = (Math.random() * 10086).toString(36).substr(0, 8)
+          const pastePlaceText = `![uploading...](${randomString})\n`
+          editor.replaceSelection(pastePlaceText)
+          const maxWidth = this.settings.maxWidth
+          if (this.settings.enableResize) {
+            const compressedFile = await new Promise((resolve, reject) => {
+              new Compressor(file, {
+                maxWidth: maxWidth,
+                success: resolve,
+                error: reject,
               })
+            })
 
-              file = compressedFile as File
-            }
-            const params = new URLSearchParams();
-            params.append('key', this.settings.token);
-            let dataURL = await readFileAsDataURL(file)
-            const source = JSON.stringify(dataURL).split(',')[1].split('"')[0]
-            params.append('source', source)
-            axios.post(this.settings.apiEndpoint, params)
-              .then(res => {
-                const url = objectPath.get(res.data, 'image.url')
-                const imgMarkdownText = `![](${url})`
-                this.replaceText(this.getEditor(), pastePlaceText, imgMarkdownText)
-              }, err => {
-                new Notice(err, 5000)
-                // console.log(err)
-                const dataTransfer = new DataTransfer();
-                dataTransfer.items.add(file);
-                const newEvt = new ClipboardEvent("paste", {
-                  clipboardData: dataTransfer
-                })
-                originalPasterHandler.paste(_, newEvt)
-              })
+            file = compressedFile as File
           }
-        }
-        else {
-          new Notice("Chevereto Image Uploader: Please check the chevereto settings.");
-          originalPasterHandler.paste(_, e);
+          const params = new URLSearchParams();
+          params.append('key', this.settings.token);
+          let dataURL = await readFileAsDataURL(file)
+          const source = JSON.stringify(dataURL).split(',')[1].split('"')[0]
+          params.append('source', source)
+          axios.post(this.settings.apiEndpoint, params)
+            .then(res => {
+              const url = objectPath.get(res.data, 'image.url')
+              const imgMarkdownText = `![](${url})`
+              this.replaceText(editor, pastePlaceText, imgMarkdownText)
+            }, err => {
+              new Notice(err, 5000)
+              console.log(err)
+            })
         }
       }
-    })
+      else {
+        new Notice("Chevereto Image Uploader: Please check the chevereto settings.");
+      }
+    }))
   }
 
+
+  // Function to replace text
   private replaceText(editor: Editor, target: string, replacement: string): void {
     target = target.trim()
     const lines = editor.getValue().split("\n");
@@ -134,18 +101,14 @@ export default class ImageUploader extends Plugin {
   }
 
   async onload(): Promise<void> {
-    // console.log("loading Image Uploader");
+    console.log("loading Image Uploader");
     await this.loadSettings();
     this.setupPasteHandler()
     this.addSettingTab(new ImageUploaderSettingTab(this.app, this));
   }
 
   onunload(): void {
-    this.cmAndHandlersMap.forEach((hander, cm) => {
-      (cm as any)._handlers.paste[0] = hander.paste;
-    })
-    // console.log("unloading Image Uploader");
-
+    console.log("unloading Image Uploader");
   }
 
   async loadSettings(): Promise<void> {
